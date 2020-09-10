375d0ad1b024ec472b99ede39cec0ab119ba03a5
Ebrahim Gabajiwala <ebrahimgabajiwala@gmail.com>
2020-09-10 13:41:30 +0530

    Added message and chat models

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Message(models.Model):
+    text_content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    chat = models.ForeignKey('Chat', on_delete=models.CASCADE)
+
+
+class Chat(models.Model):
+    last_active = models.DateTimeField(auto_now=True)
+
+
