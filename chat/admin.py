8e0054b609f1bd97ae72466b390e72b431850130
Ebrahim Gabajiwala <ebrahimgabajiwala@gmail.com>
2020-09-10 15:41:01 +0530

    Extended user model for Customer and AdminUser

--- chat/admin.py
+++ chat/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Message,Chat 
+from .models import Message, Chat, Customer, AdminUser 
 
 admin.site.register(Message)
 admin.site.register(Chat)
+admin.site.register(Customer)
+admin.site.register(AdminUser)
--- chat/models.py
+++ chat/models.py
@@ -1,14 +1,32 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
 
 class Message(models.Model):
     text_content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    chat = models.ForeignKey('Chat', on_delete=models.CASCADE)
+    chat = models.ForeignKey(
+        'Chat', 
+        on_delete=models.CASCADE, 
+        related_name='messages')
+    sender = models.ForeignKey('Customer', on_delete=models.CASCADE)
 
 
 class Chat(models.Model):
     last_active = models.DateTimeField(auto_now=True)
+    participants = models.ManyToManyField('Customer')
 
 
+class Customer(models.Model):
+    user = models.OneToOneField(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name='customer_profile')
+    contacts = models.ManyToManyField(to='self')
+
+
+class AdminUser(models.Model):
+    user = models.OneToOneField(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name='admin_profile')
